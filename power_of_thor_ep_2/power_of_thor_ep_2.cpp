d655f23737154fbf87085f88c399b66e907a41b6
Dmitry Mamylin <dmamylin@gmail.com>
2019-09-30 02:02:43 +0500

    Refactor power_of_thor_ep_2 & use BFS to find distances. Cannot pass test 'Queuing, 1 strike'

--- power_of_thor_ep_2/power_of_thor_ep_2.cpp
+++ power_of_thor_ep_2/power_of_thor_ep_2.cpp
@@ -1,19 +1,15 @@
 #include <algorithm>
+#include <cmath>
+#include <cstdint>
 #include <exception>
 #include <iostream>
-#include <cmath>
 #include <limits>
 #include <memory>
+#include <queue>
 #include <string>
 #include <vector>
 
-#pragma region("GAME CONSTANTS")
-constexpr int STRIKE_RADIUS = 3;
-constexpr int MAX_MAP_X = 40;
-constexpr int MIN_MAP_X = 0;
-constexpr int MAX_MAP_Y = 18;
-constexpr int MIN_MAP_Y = 0;
-#pragma endregion
+#define PRINT_DEBUG_INFO
 
 #pragma region("INPUT UTILS")
 template <typename T>
@@ -30,7 +26,7 @@ constexpr int INF = std::numeric_limits<int>::max();
 struct Point {
     int X = 0;
     int Y = 0;
-    
+
     static Point FromStream(std::istream& input) {
         const auto x = Read<int>(input);
         const auto y = Read<int>(input);
@@ -63,14 +59,52 @@ std::ostream& WritePoint(std::ostream& os, const Point& p, const std::string& ms
     os << p.X << "; " << p.Y;
     return os;
 }
+
+template <typename T>
+class Matrix {
+public:
+    Matrix(size_t columns, size_t rows)
+        : Columns(columns)
+        , Rows(rows)
+        , Data(Columns * Rows)
+    {
+    }
+
+    Matrix(size_t columns, size_t rows, const T& defaultValue)
+        : Columns(columns)
+        , Rows(rows)
+        , Data(Columns * Rows, defaultValue)
+    {
+    }
+
+    const T& Get(size_t column, size_t row) const {
+        return Data[AsRawIndex(column, row)];
+    }
+
+    void Set(size_t column, size_t row, const T& value) {
+        Data[AsRawIndex(column, row)] = value;
+    }
+
+    void Clear(const T& value) {
+        std::fill(Data.begin(), Data.end(), value);
+    }
+
+private:
+    using DataHolderType = std::vector<T>;
+
+private:
+    size_t Columns;
+    size_t Rows;
+    DataHolderType Data;
+
+private:
+    size_t AsRawIndex(size_t column, size_t row) const {
+        return static_cast<size_t>(row * Columns + column);
+    }
+};
 #pragma endregion
 
 #pragma region("GAME-SPECIFIC UTILS")
-bool IsOnMap(const Point& p) {
-    return p.X >= MIN_MAP_X && p.X < MAX_MAP_X
-           && p.Y >= MIN_MAP_Y && p.Y < MAX_MAP_Y;
-}
-
 const std::vector<Point>& GetPossibleDirections() {
     static const std::vector<Point> directions = {
         {-1, -1},
@@ -89,13 +123,13 @@ const std::vector<Point>& GetPossibleDirections() {
 std::string GetSymbolicDirection(const Point& current, const Point& desired) {
     std::string result;
     const auto dir = desired - current;
-    
+
     if (dir.Y > 0) {
         result += "S";
     } else if (dir.Y < 0) {
         result += "N";
     }
-    
+
     if (dir.X > 0) {
         result += "E";
     } else if (dir.X < 0) {
@@ -113,46 +147,52 @@ std::string GetSymbolicDirection(const Point& current, const Point& desired) {
 #pragma region("GAME ENTITIES")
 class Thor {
 public:
-    Thor(const Point& position, int strikesLeft)
+    Thor(const Point& position, int strikeRadius, int strikesLeft)
         : Position(position)
+        , StrikeRadius(strikeRadius)
         , StrikesLeft(strikesLeft)
     {
     }
-    
+
+    const Point GetPosition() const {
+        return Position;
+    }
+
+    int GetStrikes() const {
+        return StrikesLeft;
+    }
+
+    bool CanStrike(const Point& position) const {
+        return Distance(position, Position) <= StrikeRadius;
+    }
+
     void SetPosition(const Point& position) {
         Position = position;
     }
-    
+
     void Strike() {
         if (StrikesLeft <= 0) {
             throw std::runtime_error("Not enough charges!");
         }
         --StrikesLeft;
     }
-    
-    const Point GetPosition() const {
-        return Position;
-    }
-    
-    int GetStrikes() const {
-        return StrikesLeft;
-    }
 
 private:
     Point Position;
+    const int StrikeRadius;
     int StrikesLeft = 0;
 };
 
-class Monster {
+class Giant {
 public:
-    using ListType = std::vector<Monster>;
+    using ListType = std::vector<Giant>;
 
 public:
-    explicit Monster(const Point& position)
+    explicit Giant(const Point& position)
         : Position(position)
     {
     }
-    
+
     const Point& GetPosition() const {
         return Position;
     }
@@ -160,147 +200,256 @@ public:
 private:
     Point Position;
 };
+
+class GameWorldMap {
+public:
+    enum class CellType : uint16_t {
+        EMPTY,
+        THOR,
+        GIANT
+    };
+
+public:
+    GameWorldMap(int mapWidth, int mapHeight)
+        : MapWidth(mapWidth)
+        , MapHeight(mapHeight)
+        , Cells(static_cast<size_t>(MapWidth), static_cast<size_t>(MapHeight), CellType::EMPTY)
+    {
+    }
+
+    int GetMapWidth() const {
+        return MapWidth;
+    }
+
+    int GetMapHeight() const {
+        return MapHeight;
+    }
+
+    CellType GetEntity(const Point& position) const {
+        return Cells.Get(static_cast<size_t>(position.X), static_cast<size_t>(position.Y));
+    }
+
+    bool IsOnMap(const Point& position) const {
+        return (position.X >= 0 && position.X < MapWidth) && (position.Y >= 0 && position.Y < MapHeight);
+    }
+
+    bool HasGiant(const Point& position) const {
+        return GetEntity(position) == CellType::GIANT;
+    }
+
+    void Clear(const Point& position) {
+        Set(position, CellType::EMPTY);
+    }
+
+    void Clear() {
+        Cells.Clear(CellType::EMPTY);
+    }
+
+    void PlaceThor(const Point& position) {
+        Set(position, CellType::THOR);
+    }
+
+    void PlaceGiant(const Point& position) {
+        Set(position, CellType::GIANT);
+    }
+
+private:
+    const int MapWidth;
+    const int MapHeight;
+    Matrix<CellType> Cells;
+
+private:
+    void Set(const Point& position, CellType value) {
+        Cells.Set(static_cast<size_t>(position.X), static_cast<size_t>(position.Y), value);
+    }
+};
 #pragma endregion
 
 #pragma region("STRATEGY")
 class IStrategy {
 public:
     virtual ~IStrategy() = default;
-    
-    virtual std::string MakeDecision(const Monster::ListType& monsters, Thor& thor) = 0;
-};
 
-class SimpleStrategy final : public IStrategy {
-public:
-    std::string MakeDecision(const Monster::ListType& monsters, Thor& thor) override {
-        if (monsters.size() == 1) {
-            const auto& monster = monsters.back();        
-            if (IsInRadius(monster.GetPosition(), thor.GetPosition(), STRIKE_RADIUS)) {
-                return "STRIKE";
-            }
-        }
-        
-        return "WAIT";
-    }
+    virtual std::string MakeDecision() = 0;
 };
 
 class FollowMostDistant : public IStrategy {
 public:
-    std::string MakeDecision(const Monster::ListType& monsters, Thor& thor) override {
-        if (monsters.empty()) {
+    FollowMostDistant(const GameWorldMap& worldMap, const Giant::ListType& giants, Thor& thor)
+        : WorldMap(worldMap)
+        , Giants(giants)
+        , Player(thor)
+    {
+    }
+
+    std::string MakeDecision() override {
+        if (Giants.empty()) {
             return "WAIT";
         }
-        
-        auto allowedPositions = GetAllowedPositions(monsters, thor);
-        std::cerr << "Allowed positions: " << allowedPositions.size() << std::endl;
+
+        auto allowedPositions = FindAllowedPositions();
         if (allowedPositions.empty()) {
-            thor.Strike();
+            Player.Strike();
             return "STRIKE";
         }
-        
-        const auto& mostDistantMonster = FindMostDistantMonster(monsters, thor);
-        if (Distance(mostDistantMonster.GetPosition(), thor.GetPosition()) <= STRIKE_RADIUS) {
-            thor.Strike();
+
+        const auto& mostDistantGiant = FindMostDistantGiant();
+        if (Player.CanStrike(mostDistantGiant.GetPosition())) {
+            Player.Strike();
             return "STRIKE";
         }
-        
-        const auto nextPosition = GetNextPosition(mostDistantMonster, allowedPositions);
-        WritePoint(std::cerr, thor.GetPosition(), "Thor") << std::endl;
-        WritePoint(std::cerr, mostDistantMonster.GetPosition(), "Monster") << std::endl;
+
+        const auto nextPosition = FindNextPosition(mostDistantGiant, allowedPositions);
+        WritePoint(std::cerr, Player.GetPosition(), "Thor") << std::endl;
+        WritePoint(std::cerr, mostDistantGiant.GetPosition(), "Giant") << std::endl;
         WritePoint(std::cerr, nextPosition, "Next position") << std::endl;
-        
-        return MoveThor(nextPosition, thor);
+
+        return MoveThor(nextPosition);
     }
-    
+
 private:
-    std::string MoveThor(const Point& nextPosition, Thor& thor) {
-        const auto dir = GetSymbolicDirection(thor.GetPosition(), nextPosition);
-        thor.SetPosition(nextPosition);
+    using DistanceMap = Matrix<int>;
+
+private:
+    const GameWorldMap& WorldMap;
+    const Giant::ListType& Giants;
+    Thor& Player;
+
+private:
+    std::string MoveThor(const Point& nextPosition) {
+        const auto dir = GetSymbolicDirection(Player.GetPosition(), nextPosition);
+        Player.SetPosition(nextPosition);
         return dir;
     }
 
-    Point GetNextPosition(const Monster& mostDistantMonster, std::vector<Point>& allowedPositions) {
-        const auto& monsterPosition = mostDistantMonster.GetPosition();
-        std::sort(allowedPositions.begin(), allowedPositions.end(), [&](const Point& lhs, const Point& rhs) {
-            const auto lhsDistance = Distance(lhs, monsterPosition);
-            const auto rhsDistance = Distance(rhs, monsterPosition);
-            return lhsDistance < rhsDistance;
-        });
-        
-        for (const auto& pos : allowedPositions) {
-            WritePoint(std::cerr, pos, "Point") << ", cost: " << Distance(pos, monsterPosition) << std::endl;
+    Point FindNextPosition(const Giant& mostDistantGiant, std::vector<Point>& allowedPositions) const {
+        const auto& playerPosition = Player.GetPosition();
+        const auto distances = FindDistancesToGiant(mostDistantGiant);
+        if (distances.Get(playerPosition.X, playerPosition.Y) != INF) {
+            std::sort(allowedPositions.begin(), allowedPositions.end(), [&](const Point& lhs, const Point& rhs) {
+                return distances.Get(lhs.X, lhs.Y) < distances.Get(rhs.X, rhs.Y);
+            });
+        } else {
+            const auto& giantPosition = mostDistantGiant.GetPosition();
+            std::sort(allowedPositions.begin(), allowedPositions.end(), [&](const Point& lhs, const Point& rhs) {
+                return Distance(lhs, giantPosition) < Distance(rhs, giantPosition);
+            });
         }
-        
+
         return allowedPositions.front();
     }
 
-    static std::vector<Point> GetAllowedPositions(const Monster::ListType& monsters, const Thor& thor) {
-        std::vector<Point> result;
-        const auto playerPosition = thor.GetPosition();
-        
+    bool HasAdjacentGiants(const Point& position) const {
         for (const auto& dir : GetPossibleDirections()) {
-            const auto nextPosition = playerPosition + dir;
-            if (!IsOnMap(nextPosition)) {
+            const auto adjacentPosition = position + dir;
+            if (!WorldMap.IsOnMap(adjacentPosition)) {
                 continue;
             }
-            
-            auto minDistance = INF;
-            for (const auto& monster : monsters) {
-                const auto monsterPosition = monster.GetPosition();
-                const auto distance = Distance(nextPosition, monsterPosition);
-                if (distance < minDistance) {
-                    minDistance = distance;
-                }
+            if (WorldMap.HasGiant(adjacentPosition)) {
+                return true;
             }
-            
-            if (minDistance > 1) {
+        }
+        return false;
+    }
+
+    std::vector<Point> FindAllowedPositions() const {
+        std::vector<Point> result;
+        const auto playerPosition = Player.GetPosition();
+        for (const auto& dir : GetPossibleDirections()) {
+            const auto nextPosition = playerPosition + dir;
+            if (!WorldMap.IsOnMap(nextPosition)) {
+                continue;
+            }
+            if (!HasAdjacentGiants(nextPosition)) {
                 result.push_back(nextPosition);
             }
         }
-        
         return result;
     }
 
-    static const Monster& FindMostDistantMonster(const Monster::ListType& monsters, const Thor& thor) {
+    const Giant& FindMostDistantGiant() const {
         size_t maxIdx = 0;
         int maxDistance = 0;
-        for (size_t i = 0; i < monsters.size(); ++i) {
-            const auto& monster = monsters[i];
-            const int distanceToMonster = Distance(monster.GetPosition(), thor.GetPosition());
-            if (maxDistance < distanceToMonster) {
-                maxDistance = distanceToMonster;
+        for (size_t i = 0; i < Giants.size(); ++i) {
+            const auto& giant = Giants[i];
+            const int distanceToGiant = Distance(giant.GetPosition(), Player.GetPosition());
+            if (maxDistance < distanceToGiant) {
+                maxDistance = distanceToGiant;
                 maxIdx = i;
             }
         }
-        return monsters[maxIdx];
+        return Giants[maxIdx];
+    }
+
+    DistanceMap FindDistancesToGiant(const Giant& giant) const {
+        using VisitedMap = Matrix<uint8_t>;
+
+        DistanceMap distances(WorldMap.GetMapWidth(), WorldMap.GetMapHeight(), INF);
+        VisitedMap visited(WorldMap.GetMapWidth(), WorldMap.GetMapHeight(), false);
+        std::queue<Point> toVisit;
+        const auto giantPosition = giant.GetPosition();
+
+        const auto setDistance = [&](const Point& position, int distance) {
+            distances.Set(position.X, position.Y, distance);
+            visited.Set(position.X, position.Y, true);
+        };
+
+        const auto findToVisit = [&, this](const Point& position) {
+            for (const auto& candidateDir : GetPossibleDirections()) {
+                const auto candidateToVisit = position + candidateDir;
+                if (!WorldMap.IsOnMap(candidateToVisit) || visited.Get(candidateToVisit.X, candidateToVisit.Y)) {
+                    continue;
+                }
+                setDistance(candidateToVisit, distances.Get(position.X, position.Y) + 1);
+                if (!HasAdjacentGiants(candidateToVisit)) {
+                    toVisit.push(candidateToVisit);
+                }
+            }
+        };
+
+        setDistance(giantPosition, 0);
+        for (const auto& dir : GetPossibleDirections()) {
+            const auto nextPosition = giantPosition + dir;
+            if (!WorldMap.IsOnMap(nextPosition) || visited.Get(nextPosition.X, nextPosition.Y)) {
+                continue;
+            }
+            setDistance(nextPosition, 1);
+            findToVisit(nextPosition);
+        }
+
+        while (!toVisit.empty()) {
+            const auto& next = toVisit.front();
+            findToVisit(next);
+            toVisit.pop();
+        }
+
+        return distances;
     }
 };
 
-std::unique_ptr<IStrategy> CreateSimpleStrategy() {
-    return std::make_unique<SimpleStrategy>();
-}
-
-std::unique_ptr<IStrategy> CreateMainStrategy() {
-    return std::make_unique<FollowMostDistant>();
+std::unique_ptr<IStrategy> CreateMainStrategy(const GameWorldMap& worldMap, const Giant::ListType& giants, Thor& thor) {
+    return std::make_unique<FollowMostDistant>(worldMap, giants, thor);
 }
 #pragma endregion
 
 class World {
 public:
     explicit World(std::istream& input)
-        : Player(ReadPlayer(input))
-        , Monsters(ReadMonsters(input))
-        , Strategy(CreateMainStrategy())
+        : Player(ReadThor(input))
+        , Giants(ReadGiants(input))
+        , WorldMap(MAX_MAP_X, MAX_MAP_Y)
+        , Strategy(CreateMainStrategy(WorldMap, Giants, Player))
     {
     }
 
     void NextStep(std::istream& input, std::ostream& output) {
-        output << Strategy->MakeDecision(Monsters, Player) << std::endl;
-        
+        FillWorldMap();
+        DumpWorldMap(std::cerr);
+        output << Strategy->MakeDecision() << std::endl;
+        ClearWorldMap();
+
         Read<int>(input); // skip the remaining number of hammer strikes
-        Monsters = ReadMonsters(input);
-        
-        WriteWorldMap(std::cerr);
+        Giants = ReadGiants(input);
     }
 
     bool IsRunning() const {
@@ -308,46 +457,67 @@ public:
     }
 
 private:
+    static constexpr int THOR_STRIKE_RADIUS = 3;
+    static constexpr int MAX_MAP_X = 40;
+    static constexpr int MAX_MAP_Y = 18;
+
     Thor Player;
-    Monster::ListType Monsters;
+    Giant::ListType Giants;
+    GameWorldMap WorldMap;
     std::unique_ptr<IStrategy> Strategy;
 
 private:
-    static Thor ReadPlayer(std::istream& input) {
+    static Thor ReadThor(std::istream& input) {
         const auto position = Point::FromStream(input);
         const auto strikesLeft = Read<int>(input);
-        return {position, strikesLeft};
+        return {position, THOR_STRIKE_RADIUS, strikesLeft};
     }
 
-    static Monster::ListType ReadMonsters(std::istream& input) {
+    static Giant::ListType ReadGiants(std::istream& input) {
         const int amount = Read<int>(input);
-        Monster::ListType result;
+        Giant::ListType result;
         for (int i = 0; i < amount; ++i) {
             result.emplace_back(Point::FromStream(input));
         }
         return result;
     }
-    
-    void WriteWorldMap(std::ostream& os) const {
-        static const auto toPlainIdx = [](const Point& p) {
-            return MAX_MAP_X * p.Y + p.X;
-        };
-        
-        std::vector<char> worldMap(MAX_MAP_X * MAX_MAP_Y);
-        std::fill(worldMap.begin(), worldMap.end(), '.');
-        for (const auto& monster : Monsters) {
-            worldMap[toPlainIdx(monster.GetPosition())] = 'G';
+
+    void FillWorldMap() {
+        WorldMap.PlaceThor(Player.GetPosition());
+        for (const auto& giant : Giants) {
+            WorldMap.PlaceGiant(giant.GetPosition());
         }
-        worldMap[toPlainIdx(Player.GetPosition())] = 'T';
-        
-        os << "*" << std::string(2 * MAX_MAP_X - 1, '-') << "*\n|";
-        for (size_t i = 0; i < worldMap.size(); ++i) {
-            if (i != 0 && i % MAX_MAP_X == 0) {
-                os << "\n|";
+    }
+
+    void ClearWorldMap() {
+        WorldMap.Clear();
+    }
+
+    void DumpWorldMap(std::ostream& os) const {
+        #ifdef PRINT_DEBUG_INFO
+        static const auto renderEntity = [](const GameWorldMap::CellType& type) {
+            switch (type) {
+                case GameWorldMap::CellType::EMPTY: return '.';
+                case GameWorldMap::CellType::THOR: return 'T';
+                case GameWorldMap::CellType::GIANT: return 'G';
+                default: return '?';
             }
-            os << worldMap[i] << "|";
+        };
+
+        os << "*" << std::string(2 * MAX_MAP_X - 1, '-') << "*\n|";
+        for (int y = 0; y < MAX_MAP_Y; ++y) {
+            bool isNewRow = y != 0;
+            for (int x = 0; x < MAX_MAP_X; ++x) {
+                if (isNewRow) {
+                    os << "\n|";
+                    isNewRow = false;
+                }
+                const auto entity = WorldMap.GetEntity({x, y});
+                os << renderEntity(entity) << "|";
+            }
         }
         os << "\n*" << std::string(2 * MAX_MAP_X - 1, '-') << "*" << std::endl;
+        #endif
     }
 };
 
